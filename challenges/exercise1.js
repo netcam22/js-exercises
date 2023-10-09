4a561cb51a0d4fc97d0058257d2e065efa4e53c1
netcam22 <annettelesage@btinternet.com>
2023-10-09 18:09:23 +0100

    complete exercise 1

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,24 +1,27 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  return arr.reduce(
+    (count, animal) => (animal === "sheep" ? count + 1 : count),
+    0
+  );
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return /M{1}\d+/.test(person.address.postCode);
 }
