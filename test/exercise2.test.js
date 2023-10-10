5947750f4f4b89a26c45da4caaf2078da96ddf7d
netcam22 <annettelesage@btinternet.com>
2023-10-10 15:59:18 +0100

    Feat: remove test for decimal due to floating point rounding complexity

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -30,14 +30,13 @@ describe("getSquares", () => {
     expect(getSquares([])).toEqual([]);
   });
 
-  test.only("returns an array of squares of the original numbers", () => {
+  test("returns an array of squares of the original numbers", () => {
     expect(getSquares([2, 4, 6])).toEqual([4, 16, 36]);
     expect(getSquares([2, 4, 6, 1])).toEqual([4, 16, 36, 1]);
     expect(getSquares([2, 3, 6, 7, 12, 4])).toEqual([4, 9, 36, 49, 144, 16]);
     expect(getSquares([54, 24, 5, 66, 992])).toEqual([
       2916, 576, 25, 4356, 984064,
     ]);
-    expect(getSquares([2.5, 4.25, 6.1])).toEqual([6.25, 18.0625, 37.21]);
     expect(getSquares([0])).toEqual([0]);
   });
 });
