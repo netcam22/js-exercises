e109a126e9248a15ed4e1c2ae303170b32017bfb
netcam22 <annettelesage@btinternet.com>
2023-10-14 13:32:39 +0100

    refactor: updated all  to use arrow functions

--- challenges/exercise3.js
+++ challenges/exercise3.js
@@ -1,21 +1,17 @@
 export function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  return nums.filter(function (num) {
-    return num < 1;
-  });
+  return nums.filter(num => num < 1);
 }
 
 export function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  return names.filter(function (name) {
-    return name[0] === char;
-  });
+  return names.filter(name => name[0] === char);
 }
 
 export function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  return words.filter(function (word) {
+  return words.filter(word => {
     const wordArr = word.split(" ");
     return wordArr[0] === "to" && wordArr[1] !== null;
   });
@@ -23,36 +19,28 @@ export function findVerbs(words) {
 
 export function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  return nums.filter(function (num) {
-    return Number.isInteger(num);
-  });
+  return nums.filter(num => Number.isInteger(num));
 }
 
 export function getCities(users) {
   if (!users) throw new Error("users is required");
-  return users.map(function (user) {
-    return user.data.city.displayName;
-  });
+  return users.map(user => user.data.city.displayName);
 }
 
 export function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  return nums.map(function (num) {
-    return parseFloat(Math.sqrt(num).toFixed(2));
-  });
+  return nums.map(num => parseFloat(Math.sqrt(num).toFixed(2)));
 }
 
 export function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  return sentences.filter(function (sentence) {
-    return sentence.toLowerCase().includes(str.toLowerCase());
-  });
+  return sentences.filter(sentence =>
+    sentence.toLowerCase().includes(str.toLowerCase())
+  );
 }
 
 export function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  return triangles.map(function (triangle) {
-    return Math.max(...triangle);
-  });
+  return triangles.map(triangle => Math.max(...triangle));
 }
